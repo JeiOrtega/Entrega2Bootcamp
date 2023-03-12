740efd1c6791fba855af93e8584335bcc7c9072b
jeisonOrtega <jeisonempleo1@gmail.com>
2023-03-12 10:40:14 -0300

    Entrega del ejercicio #2

---
+++ index.js
@@ -0,0 +1,14 @@
+const  mi_informacion = {
+    nombre: "Jeison",
+    edad: 24,
+    "¿Eres-desarrollador?" : true,
+    "Fecha-de-nacimineto": new Date (1998, 3, 26),
+    "libro-favorito" : {
+        titulo: "Ser digital",
+        autor: "Nicholas Negroponte",
+        fecha: 1995,
+        url: "https://es.wikipedia.org/wiki/Ser_digital"
+    }
+}
+
+console.log(mi_informacion)
